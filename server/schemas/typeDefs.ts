00f2f3dc7318c806e0304d1d3146a69a526bff65
Fortunate122 <davidsaldana122@gmail.com>
2025-05-29 21:10:36 -0700

    added JournalEntry typedefs and resolvers

--- server/schemas/typeDefs.ts
+++ server/schemas/typeDefs.ts
@@ -18,16 +18,34 @@ const typeDefs = gql`
     updatedAt: String
   }
 
+  type JournalEntry {
+    _id: ID!
+    savedPlantId: ID!
+    userId: ID!
+    date: String!
+    status: String
+    notes: String
+    image: String
+    createdAt: String
+    updatedAt: String
+  }
+
   type Query {
     me: User
     getSavedPlantsByUser(userId: ID!): [SavedPlant]
     getSavedPlantById(id: ID!): SavedPlant
+    getJournalEntriesByPlant(savedPlantId: ID!): [JournalEntry]
+    getJournalEntryById(id: ID!): JournalEntry
   }
 
   type Mutation {
     addSavedPlant(trefleId: String!, nickname: String, location: String, imageUrl: String): SavedPlant
     removeSavedPlant(id: ID!): SavedPlant
     updateSavedPlant(id: ID!, nickname: String, location: String): SavedPlant
+
+    addJournalEntry(savedPlantId: ID!, date: String!, status: String, notes: String, image: String): JournalEntry
+    updateJournalEntry(id: ID!, status: String, notes: String, image: String): JournalEntry
+    deleteJournalEntry(id: ID!): JournalEntry
   }
 `;
 
