b53cd40e227287c6f1fdf80d12d8bb056271b0b9
Fortunate122 <davidsaldana122@gmail.com>
2025-06-05 19:11:15 -0700

    added app.use logger to server.ts

--- server/server.ts
+++ server/server.ts
@@ -6,9 +6,7 @@ import connectDB from './config/db.js';
 import typeDefs from './schemas/typeDefs.js';
 import resolvers from './schemas/resolvers.js';
 import { authMiddleware } from './utils/auth.js';
-
 import logger from './middleware/logger.js';
-
 import favoriteRoutes from './routes/api/favoritesRoutes.js';
 
 declare global {
@@ -21,10 +19,15 @@ declare global {
 
 config();
 const app = express();
-const PORT = process.env.PORT || 4000;app.use(cors());
+const PORT = process.env.PORT || 4000;
+
+app.use(logger);
+app.use(cors());
 app.use(express.json());
 app.use(authMiddleware);
-app.use('/api/favorites', favoriteRoutes);const server = new ApolloServer({
+app.use('/api/favorites', favoriteRoutes);
+
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ user: req.user })
@@ -32,7 +35,9 @@ app.use('/api/favorites', favoriteRoutes);const server = new ApolloServer({
 
 const startServer = async () => {
   await server.start();
-  server.applyMiddleware({ app });  connectDB();
+  server.applyMiddleware({ app });  
+  
+  connectDB();
   app.listen(PORT, () => {
     console.log(`:rocket: Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   });
