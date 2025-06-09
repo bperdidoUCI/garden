d03d6f25feb507eef97478c40a180294fb3852b7
Thays Ribeiro Maher <thaysmoiaribeiro@gmail.com>
2025-06-09 09:35:23 +0000

    fix: update frontend server port to 10000 in Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000, // Change this to your desired port for the frontend
+    port: 10000, // Change this to your desired port for the frontend
     proxy: {
       '/api': {
         target: 'http://localhost:10000', // Change this to your backend server URL
