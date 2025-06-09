0900a0ce1f41af5093b063681fc526088df4b8d2
Thays Ribeiro Maher <thaysmoiaribeiro@gmail.com>
2025-06-09 09:43:06 +0000

    fix: update proxy target in Vite configuration to match backend server URL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000, // Change this to your desired port for the frontend
+    port: 10000,
+    open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:10000', // Change this to your backend server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
+        target: 'http://localhost:4000',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
+})
