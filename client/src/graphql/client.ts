16474480e27b4bd912361711e73b4001c1bb973b
Fortunate122 <davidsaldana122@gmail.com>
2025-06-09 18:24:35 -0700

    debug

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_URI,
+  uri: import.meta.env.VITE_GRAPHQL_URI || 'http://localhost:10000/graphql',
   cache: new InMemoryCache(),
 });
 
